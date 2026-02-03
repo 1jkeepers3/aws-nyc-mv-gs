a510856aab6e32994f9af0a13c10d7b2004721ed
Joseph Keepers <josephkeepers@gmail.com>
2026-02-03 11:31:42 -0500

    updating app.js

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60, // 1 hour
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: false,
       sameSite: "lax",
     },
   })
